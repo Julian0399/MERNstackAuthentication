e6e7d7a41f32a1ae7f791c1ec9b7d1ef673062cf
julianrod <julishalo99@hotmail.com>
2025-03-01 00:08:14 -0500

    Feature: connection with DB in Atlas MongoDb

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,36 @@
-const server = async () => {
-    console.log("Server is running")
-}
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./src/db/connect.js";
+import cookieParser from "cookie-parser";
+import fs from "node:fs";
 
-server()
+dotenv.config();
+const port = process.env.PORT || 8000;
+
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+//routes
+const routeFiles = fs.readdirSync("./src/routes");
+console.log(routeFiles);
+
+
+const server = async () => {
+  try {
+    await connectDB();
+    app.listen(port, () => {
+      console.log("Server is running on port: ", port);
+    });
+  } catch (error) {
+    console.log("Error in server: ", error.message);
+    process.exit(1);
+  }
+};
+
+server();
